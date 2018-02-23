87c127aa40fd57948aa102c8fa60cb39032fe7cf
timur.ab <timur.ab@gmail.com>
2018-02-23 16:53:44 +0600

    del doc gparser

--- sqlparser/gparser/doc/html/search/all_61.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['args',['args',['../structgsp__call_sql_node.html#a49a91e358008875bbb9d2867d30d48bc',1,'gsp_callSqlNode']]]
-];
--- sqlparser/gparser/doc/html/search/all_72.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['rolenamelist',['roleNameList',['../structgsp__drop_role_sql_node.html#aea002dbf47225ad82e98213f83ed7a89',1,'gsp_dropRoleSqlNode']]]
-];

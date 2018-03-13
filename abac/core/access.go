d1deaf88e6f6459886bef35f15b6b99ff95e52c7
timur.ab <timur.ab@gmail.com>
2018-03-14 00:30:00 +0600

    тестовое использоваение casbin

--- abac/core/access.go
+++ abac/core/access.go
@@ -3,14 +3,20 @@ package core
 import (
 	"log"
 
-	//cb "github.com/casbin/casbin"
+	cb "github.com/casbin/casbin"
 	pb "ps.kz/aob/abac/protos"
 )
 
+func enforce(e *cb.Enforcer, sub string, obj string, act string) {
+	e.Enforce(sub, obj, act)
+}
+
 func checkAccess(userSql *pb.UserSql) {
 
-	//e := cb.NewEnforcer("")
+	e := cb.NewEnforcer("/opt/gopath/src/ps.kz/aob/abac/core/abac_model_policy.conf", "/opt/gopath/src/ps.kz/aob/abac/core/policy.csv")
 	for _, tableColumn := range userSql.Obj.TableColumns {
-		log.Printf("/n tableColumn=%v", tableColumn)
+		log.Printf("\ntableColumn=%v", tableColumn)
+
+		enforce(e, userSql.GetUser(), tableColumn, "read")
 	}
 }
